1907eb1d344f04a0126f0538409db20ea2e9ceb6
jroblar <jroblar98@gmail.com>
2025-09-26 21:30:38 -0600

    initial-commit

---
+++ ssp_modeling/output_postprocessing/postprocessing_250820.r
@@ -0,0 +1,27 @@
+#################################################
+# Post processing process
+#################################################
+
+# load packages
+library(data.table)
+library(reshape2)
+library(mFilter)
+library(ggplot2)
+
+rm(list=ls())
+
+#ouputfile
+dir.output  <- "ssp_modeling/ssp_run_output/sisepuede_results_sisepuede_run_2025-09-26T19;43;09.815150/"
+output.file <- "sisepuede_results_sisepuede_run_2025-09-26T19;43;09.815150_WIDE_INPUTS_OUTPUTS.csv"
+
+region <- "morocco" 
+iso_code3 <- "MAR"
+
+year_ref <- 2022
+
+source('ssp_modeling/output_postprocessing/scr/run_script_baseline_run_new.r')
+
+source('ssp_modeling/output_postprocessing/scr/data_prep_new_mapping.r')
+
+source('ssp_modeling/output_postprocessing/scr/data_prep_drivers.r')
+
